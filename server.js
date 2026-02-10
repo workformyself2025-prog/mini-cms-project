f729908ae9727872924bc4ff590be3b226bb32db
workformyself2025-prog <workformyself2025@gmail.com>
2026-02-10 21:22:02 +0530

    fixed port for deployment

--- server.js
+++ server.js
@@ -148,7 +148,10 @@ app.delete("/blogs/:id", async (req, res) => {
 
 
 // 🔹 Server Start
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
